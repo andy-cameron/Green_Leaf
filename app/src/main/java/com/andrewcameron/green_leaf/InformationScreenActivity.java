6471567a7cbc5fec1e7a6b6b7e47b435fd549049
andy-cameron <acameron851@gmail.com>
2019-03-23 20:56:24 +0000

    email updatable

--- app/src/main/java/com/andrewcameron/green_leaf/InformationScreenActivity.java
+++ app/src/main/java/com/andrewcameron/green_leaf/InformationScreenActivity.java
@@ -2,14 +2,23 @@ package com.andrewcameron.green_leaf;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class InformationScreenActivity extends AppCompatActivity {
 
@@ -21,8 +30,8 @@ public class InformationScreenActivity extends AppCompatActivity {
     private String uID;
 
     private TextView profileName;
-    private TextView profileEmail;
-    private TextView profileOrganisation;
+    private EditText profileEmail;
+    private EditText profileOrganisation;
     private TextView profileLeaves;
 
     @Override
@@ -30,7 +39,37 @@ public class InformationScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_screen);
 
+
         Button returnToProfileScreen = (Button) findViewById(R.id.return_to_profile);
+        Button editEmail = (Button) findViewById(R.id.edit_email_button);
+        profileName = findViewById(R.id.profile_name);
+        profileEmail = findViewById(R.id.profile_email);
+
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (mUser != null) {
+            uID = mUser.getUid();
+        }
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Profiles").child(uID);
+
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Setting name field
+                String firstName = (String) dataSnapshot.child("firstName").getValue();
+                String lastName = (String) dataSnapshot.child("lastName").getValue();
+                profileName.setText(firstName + " " + lastName);
+
+                //Setting Email field
+                String email = (String) dataSnapshot.child("email").getValue();
+                profileEmail.setHint(email);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         returnToProfileScreen.setOnClickListener((new View.OnClickListener() {
             @Override
@@ -39,5 +78,20 @@ public class InformationScreenActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         }));
+
+        editEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mUser.updateEmail(profileEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User email address updated.");
+                            mDatabase.child("email").setValue(profileEmail.getText().toString());
+                        }
+                    }
+                });
+            }
+        });
     }
 }
